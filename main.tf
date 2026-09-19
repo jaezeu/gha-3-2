d2214263ad166002d58abf0ba10470fb1d49313d
jaezeu <jazeel.meerasah@gmail.com>
2026-09-19 08:44:07 +0800

    Adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket       = "sctp-tfstate-ce13"
+    key          = "jaz-32-state-key.tfstate"
+    region       = "us-east-1"
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "workshop" {
+  bucket_prefix = "jaz-32-bucket"
+}
+
